7a02b6e7e3f40a8308e0f0e4dc7a452be6644dc0
kitanoshohei <kitano.1105.shohei2598@gmail.com>
2021-07-31 20:14:17 +0900

    7/31 learns登録機能実装

--- src/components/Postlearn.vue
+++ src/components/Postlearn.vue
@@ -15,17 +15,31 @@
         <v-container grid-list-md>
           <v-layout wrap>
             <v-flex xs12>
-              <v-text-field label="タイトル" required></v-text-field>
+              <v-text-field label="タイトル" v-model="title" required></v-text-field>
             </v-flex>
             <v-flex xs12>
-              <v-text-field label="パスワード" type="password" required></v-text-field>
+              <v-text-field label="詳細" v-model="detail" required></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field label="カテゴリー" v-model="category_id" required></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field label="言語" v-model="language_id" required></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <datetime v-model="learn_datetime_start" type="datetime" format="yyyy-MM-dd HH:mm:ss" input-id="startDate">
+                <label slot="before" for="startDate">開始時間</label>
+              </datetime>
+              <datetime v-model="learn_datetime_end" type="datetime" format="yyyy-MM-dd HH:mm:ss" input-id="endDate">
+                <label slot="before" for="endDate">終了時間</label>
+              </datetime>
             </v-flex>
           </v-layout>
         </v-container>
       </v-card-text>
       <v-card-actions>
         <v-container grid-list-md>
-          <v-btn block color="success" dark @click="dialog = false">記録</v-btn>
+          <v-btn block color="success" dark @click="postLearn">記録</v-btn>
         </v-container>
       </v-card-actions>
     </v-card>
@@ -33,13 +47,37 @@
 </template>
 
 <script>
+import { Datetime } from 'vue-datetime';
 export default {
   name: 'Postlearn',
-  data() {
-    return {
-      dialog: false
-    };
-  }
+  components: {
+    datetime: Datetime
+  },
+  data: () => ({
+    title: '',
+    detail: '',
+    category_id: '',
+    language_id: '',
+    learn_datetime_start: '',
+    learn_datetime_end: '',
+    dialog: false,
+  }),
+  methods: {
+    async postLearn() {
+      await this.axios.post('http://localhost:8888/api/learn', {
+        title: this.title,
+        detail: this.detail,
+        user_id: this.$store.state.user.user_id,
+        category_id: this.category_id,
+        language_id: this.language_id,
+        learn_datetime_start: this.learn_datetime_start,
+        learn_datetime_end: this.learn_datetime_end,
+      })
+        
+      this.dialog = false
+      this.$router.push('/calender')
+    },
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import vuetify from './plugins/vuetify';
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import store from './store'
+import { Datetime } from 'vue-datetime'
+// You need a specific loader for CSS files
+import 'vue-datetime/dist/vue-datetime.css'
 
 Vue.config.productionTip = false
 
 Vue.use(VueAxios, axios)
+Vue.use(Datetime)
 
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
